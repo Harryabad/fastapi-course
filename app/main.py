0121b900982a0c983c3fa41cb0f1bc2678e835c0
Harry <harryabad94@gmail.com>
2023-01-19 23:31:07 +0000

    updated code incorporating SQL

--- app/main.py
+++ app/main.py
@@ -55,17 +55,24 @@ def root():
 # GET all posts - Read
 @app.get("/posts") 
 def get_posts():
-    return {"data": my_posts}
+    posts = cursor.execute("""SELECT * FROM posts """) 
+    posts = cursor.fetchall()
+    return {"data": posts}
 
 # Creating a POST - Create
 @app.post("/posts", status_code=status.HTTP_201_CREATED)
 def createpost(post: Post):
-    #print(post)
-    #print(post.dict())
-    post_dict = post.dict()
-    post_dict['id'] = randrange(0, 1000000)
-    my_posts.append(post_dict)
-    return {"data": post_dict}
+    # post_dict = post.dict()
+    # post_dict['id'] = randrange(0, 1000000)
+    # my_posts.append(post_dict)
+
+    ## dont use f string here to avoid sql injection
+    cursor.execute("""INSERT INTO posts (title, content, published) VALUES (%s, %s, %s) RETURNING * """ , (post.title, post.content, post.published)) 
+    new_post = cursor.fetchone()  # staging a change and retreives the next set on results
+
+    conn.commit() ## to save data into database
+
+    return {"data": new_post}
 
 # Order Matters! - if get {id} was first would throw an integer error. FIFO
 @app.get("/posts/latest")
@@ -76,7 +83,11 @@ def get_latest_post():
 # GET an individual Post - Read
 @app.get("/posts/{id}")
 def get_post(id: int, response: Response):
-    post = find_post(id)
+
+    cursor.execute("""SELECT * FROM posts WHERE id = %s """, (str(id),)) #may need to add a comma after id
+    post = cursor.fetchone()
+    
+
     if not post:
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
                             detail=f"post with id: {id} was not found")
@@ -87,27 +98,36 @@ def get_post(id: int, response: Response):
 
 @app.delete("/posts/{id}", status_code=status.HTTP_204_NO_CONTENT)
 def delete_post(id: int):
+
+    cursor.execute("""DELETE FROM posts WHERE id = %s RETURNING * """, (str(id),))
+    deleted_post = cursor.fetchone()
+    conn.commit()
+
     # deleting post - find index in array -> my_posts.pop()
-    index = find_index_post(id)
-    if index == None:
+    #index = find_index_post(id)
+    if deleted_post == None:
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail = f"post with id {id} does not exist")
-    my_posts.pop(index)
+    #my_posts.pop(index)
     #return {'message': 'post was successfully deleted'} # will not show as a property of 204 error, shouldn't send data back
     return Response(status_code=status.HTTP_204_NO_CONTENT)
 
 @app.put("/posts/{id}") # send put request to specific ID
 def update_post(id: int, post: Post):
 
-    index = find_index_post(id) #quick check to find index 
+    cursor.execute("""UPDATE posts SET title = %s, content = %s, published = %s WHERE id = %s RETURNING * """,
+     (post.title, post.content, post.published, (str(id))))
+    
+    updated_post = cursor.fetchone()
+    conn.commit()
+    # index = find_index_post(id) #quick check to find index 
 
-    if index == None: # catch if doesn't exist
+    if updated_post== None: # catch if doesn't exist
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail = f"post with id {id} does not exist")
 
-    post_dict = post.dict() # if it does exists, take front end data and convert to python dictionary
-    post_dict['id'] = id # add the id
-    my_posts[index] = post_dict # for the post within index, replace with new post_dict  update
-
-    return {'data': post_dict}
+    # post_dict = post.dict() # if it does exists, take front end data and convert to python dictionary
+    # post_dict['id'] = id # add the id
+    # my_posts[index] = post_dict # for the post within index, replace with new post_dict  update
+    return {'data': updated_post}
     
 
 
@@ -123,4 +143,6 @@ def update_post(id: int, post: Post):
 
 
 
-### 4:08:00 on video
+### 4:31:00 on video
+### venv/Scripts/activate.bat 
+### uvicorn app.main:app --reload 
